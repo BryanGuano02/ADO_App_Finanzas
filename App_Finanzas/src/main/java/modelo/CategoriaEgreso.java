294ad411da9b13d4f73795cdfb46de5d4089bdd4
BryanGuano02 <bryan.guano@epn.edu.ec>
2024-08-08 10:21:13 -0500

    add Inicios Entidades

--- App_Finanzas/src/main/java/modelo/CategoriaEgreso.java
+++ App_Finanzas/src/main/java/modelo/CategoriaEgreso.java
@@ -1,5 +1,13 @@
 package modelo;
 
-public class CategoriaEgreso {
+import java.util.Date;
+import java.util.List;
 
+public class CategoriaEgreso extends Categoria {
+    private List<CategoriaEgreso> categoriasEgreso = null;
+//    constructor??
+
+    public List<CategoriaEgreso> obtenerTodoPorFecha(Date desde, Date hasta){
+        return null;
+    }
 }
--- App_Finanzas/src/main/java/modelo/Movimiento.java
+++ App_Finanzas/src/main/java/modelo/Movimiento.java
@@ -1,5 +1,43 @@
 package modelo;
 
-public class Movimiento {
+import java.util.Date;
+import java.util.List;
 
+public class Movimiento {
+    private String concepto;
+    private Date fecha;
+    private double valor;
+
+    public Movimiento() {
+    }
+
+    public Movimiento(String concepto, Date fecha, double valor) {
+        this.concepto = concepto;
+        this.fecha = fecha;
+        this.valor = valor;
+    }
+
+    public List<Movimiento> movimientos(Date desde, Date hasta){
+        return null;
+    }
+
+    public List<Movimiento> obtenerMovimientosPorIdCuenda(int idCuenta){
+        return null;
+    }
+
+    public Movimiento obtenerMovimientoPorIdMovimiento(int idMovimiento){
+        return null;
+    }
+
+    public void eliminarMovimiento(int idMovimiento){
+
+    }
+
+    public void guardarMovimiento(Movimiento movimiento){
+
+    }
+
+    public List<Movimiento> obtenerMovimientosPorIdCategoria(int idCategoria){
+        return null;
+    }
 }
