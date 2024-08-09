ef7ffcf37784d3f2f7261564b6234dec4194e143
EriikJG <94009110+EriikJG@users.noreply.github.com>
2024-08-08 21:03:25 -0500

    add persistencia correcto

--- App_Finanzas/src/main/java/test/ORMtest.java
+++ App_Finanzas/src/main/java/test/ORMtest.java
@@ -1,15 +1,17 @@
 package test;
 
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import modelo.entidades.Cuenta;
 
+
 public class ORMtest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistencia");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("chaucherita_PU");
 		EntityManager em = emf.createEntityManager();
 
 		Cuenta cuenta = new Cuenta(1, "Bryan", 1.2);
