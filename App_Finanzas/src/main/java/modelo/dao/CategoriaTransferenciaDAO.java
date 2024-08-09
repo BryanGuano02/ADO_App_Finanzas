d84a40a77819b0b95680b38a96abba5d8dcd3f4c
BryanGuano02 <bryan.guano@epn.edu.ec>
2024-08-08 20:26:46 -0500

    add Percistencia

---
+++ App_Finanzas/src/main/java/modelo/dao/CategoriaTransferenciaDAO.java
@@ -0,0 +1,4 @@
+package modelo.dao;
+
+public class CategoriaTransferenciaDAO {
+}
---
+++ App_Finanzas/src/main/java/test/ORMtest.java
@@ -0,0 +1,22 @@
+package test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import modelo.entidades.Cuenta;
+
+public class ORMtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistencia");
+		EntityManager em = emf.createEntityManager();
+
+		Cuenta cuenta = new Cuenta(1, "Bryan", 1.2);
+		em.getTransaction().begin();
+		em.persist(cuenta);
+		em.getTransaction().commit();
+
+	}
+
+}
